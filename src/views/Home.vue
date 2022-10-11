e1acdba3ce3def31851cb8dde43136cffab4c539
ishowman <16897283+ishowman@users.noreply.github.com>
2022-10-11 21:23:50 +0800

    feat: add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,31 @@
 <template>
-  <button @click="play()">play</button>
+  <div class="full-page" id="home">
+    <img :src="playBtn" alt="开始游戏" class="play-btn" @click="play()" />
+  </div>
 </template>
 
 <script lang="ts" setup>
-import {  useRouter } from 'vue-router';
+import { useRouter } from 'vue-router';
+import playBtn from '@/assets/index/play-btn.png';
+
 const router = useRouter();
 
 function play() {
   router.push({ name: 'Game' });
 }
 </script>
+<style scoped>
+#home {
+  background-image: url('@/assets/index/bg.png');
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: relative;
+}
+.play-btn {
+  position: absolute;
+  bottom: 15%;
+  left: 9%;
+  right: 9%;
+  width: 82%;
+}
+</style>
