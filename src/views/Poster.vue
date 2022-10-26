d1a8691e80b42e18bae0f86db4da28d6b49ca577
corinchen <corinchen@xiaoe-tech.com>
2022-10-26 14:30:13 +0800

    fix: 修改一下记录

--- src/views/Poster.vue
+++ src/views/Poster.vue
@@ -1,139 +1,250 @@
 <template>
-  <div ref="poster" v-show="!imgUrl"></div>
-  <img
-    :src="imgUrl"
-    v-if="imgUrl"
+  <!-- 点击分享-->
+  <div
     :style="{
-      width: canvasWidth,
-      height: canvasHeight,
-      zIndex: 99,
+      position: 'absolute',
+      overflowX: 'hidden',
+      width: sc.w + 'px',
+      height: sc.h + 'px',
     }"
-  />
+  >
+    <div
+      :style="{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        width: sc.w + 'px',
+        height: sc.h + 'px',
+      }"
+    >
+    <!-- todo 这里也是 -->
+      <img
+        src="poster/jin.png"
+        :style="{
+          position: 'absolute',
+          left: sc.w * 0.1411 + 'px',
+          top: sc.h * 0.17 + 'px',
+          width: sc.w * 0.2521 + 'px',
+          height: sc.h * 0.1688 + 'px',
+          zIndex: 8,
+        }"
+      />
+      <img
+        src="poster/bg.png"
+        :style="{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          width: sc.w + 'px',
+          height: sc.h + 'px',
+        }"
+      />
+      <img
+        src="poster/poster.png"
+        :style="{
+          width: parseInt(sc.w) * 0.82 + 'px',
+          height: (parseInt(sc.w) / 869) * 1543 + 'px',
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          margin: 'auto',
+          top: (parseInt(sc.w) / 869) * 1543 * 0.14 + 'px',
+        }"
+      />
+      <img
+        src="poster/get-poster-text.png"
+        :style="{
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          margin: 'auto',
+          bottom: (parseInt(sc.w) / 869) * 1543 * 0.28 + 'px',
+          width: sc.w * 0.32 + 'px',
+          height: sc.h * 0.038 + 'px',
+        }"
+      />
+      <img
+        src="poster/squre.png"
+        :style="{
+          width: sc.w * 0.58 + 'px',
+          height: sc.h * 0.089 + 'px',
+          backgroundSize: '100%',
+          bottom: (parseInt(sc.w) / 869) * 1543 * 0.16 + 'px',
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          margin: 'auto',
+        }"
+      />
+      <!-- todo 图标 金银铜 此处需要变量改变显示的金银 -->
+      <img
+        src="poster/jin.png"
+        :style="{
+          width: sc.w * 0.166 + 'px',
+          height: sc.w * 0.166 + 'px',
+          backgroundSize: '100%',
+          bottom: (parseInt(sc.w) / 869) * 1543 * 0.16 + 4 + 'px',
+          position: 'absolute',
+          left: sc.w * 0.25 + 'px',
+          margin: 'auto',
+        }"
+      />
+      <!-- todo 这里也是 -->
+      <div
+        :style="{
+          bottom: (parseInt(sc.w) / 869) * 1543 * 0.16 + 24 + 'px',
+          position: 'absolute',
+          left: sc.w * 0.425 + 'px',
+          margin: 'auto',
+          fontWeight: 500,
+          fontSize: '30.34px',
+          color: '#FFFFFF',
+        }"
+      >
+        金牌奖品
+      </div>
+      <img
+        src="poster/btn-award.png"
+        :style="{
+          position: 'absolute',
+          bottom: (parseInt(sc.w) / 869) * 1543 * 0.06 + 'px',
+          width: sc.w * 0.3829 + 'px',
+          height: sc.h * 0.0731 + 'px',
+          left: sc.w * 0.11 + 'px',
+        }"
+      />
+      <img
+        src="poster/btn-share.png"
+        :style="{
+          position: 'absolute',
+          bottom: (parseInt(sc.w) / 869) * 1543 * 0.06 + 'px',
+          right: sc.w * 0.11 + 'px',
+          width: sc.w * 0.3829 + 'px',
+          height: sc.h * 0.0731 + 'px',
+        }"
+        @click="modalShare = true"
+      />
+    </div>
+  </div>
+   <div
+      v-if="modalShare"
+      @click="modalShare = false"
+      :style="{
+        width: sc.w + 'px',
+        height: sc.h + 'px',
+        background: 'rgba(0,0,0, 0.77)',
+        position: 'fixed',
+        left: 0,
+        top: 0,
+        zIndex: 9,
+      }"
+    >
+      <img
+        src="poster/share-bg.png"
+        :style="{
+          width: sc.w * 0.67 + 'px',
+          height: sc.h * 0.3333 + 'px',
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          margin: 'auto',
+          top: sc.h * 0.0756 + 'px',
+          zIndex: 10,
+        }"
+      />
+    </div>
+  <div
+    v-if="modalStatus"
+    :style="{
+      width: sc.w + 'px',
+      height: sc.h + 'px',
+      background: 'rgba(0,0,0, 0.77)',
+      position: 'fixed',
+      left: 0,
+      top: 0,
+      zIndex: 9,
+    }"
+    @click="modalStatus = false"
+  >
+    <!-- 一张图 -->
+    <div
+      :style="{
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        margin: auto,
+        width: sc.w + 'px',
+        height: sc.h +'px',
+        top: sc.h * 0.14 + 'px',
+      }"
+    >
+      <img
+        src="poster/poaster.png"
+        :style="{
+          width: sc.w * 0.5811 + 'px',
+          height: sc.h * 0.5805 + 'px',
+          zIndex: 10,
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          margin: 'auto',
+        }"
+      />
+      <!-- 这里是变量 金 银 铜 -->
+      <img
+        src="poster/金.jpg"
+        alt=""
+        :style="{
+          width: sc.w * 0.57 + 'px',
+          height: sc.h * 0.576 + 'px',
+          zIndex: 10,
+          position: 'absolute',
+          left: 0,
+          right: 0,
+          margin: 'auto',
+          borderRadius: '32px',
+          zIndex: 11,
+          top: '2px',
+        }"
+      />
+       <img src="poster/save-long-img.png" :style="{
+         position: 'absolute',
+          left: 0,
+          right: 0,
+          margin: 'auto',
+          width: sc.w * 0.394 + 'px',
+          height: sc.h * 0.0192 + 'px',
+          top: sc.h * 0.575 + 28 + 'px',
+          zIndex: 999
+      }">
+      <img src="poster/x.png" @click="modalStatus = false" :style="{
+        position: 'absolute',
+        left: 0,
+          right: 0,
+          margin: 'auto',
+          top: sc.h * 0.575 + 60 + 'px',
+          zIndex: 999,
+          width: sc.w*0.1017 + 'px',
+          height: sc.h * 0.0508 + 'px'
+      }">
+    </div>
+
+  </div>
 </template>
 <script setup>
-import * as PIXI from 'pixi.js-legacy';
-import { Howl } from 'howler';
-import { ref, onMounted } from 'vue';
-import { useRouter } from 'vue-router';
-import playBgm from '@/assets/audios/play.mp3';
-const sound = new Howl({
-  src: playBgm,
-});
-
-const poster = ref('');
-
+import { ref, onMounted } from "vue";
+import { useRouter } from "vue-router";
+const poster = ref("");
 const router = useRouter();
-
-const imgUrl = ref('');
+const modalStatus = ref(true);
+const modalShare = ref(false);
+const sc = ref({
+  w: window.innerWidth,
+  h: window.innerHeight,
+});
+// console.log(sc.value.w, sc.value.h)
 
 onMounted(() => {
-  let Application = PIXI.Application,
-    Container = PIXI.Container,
-    resources,
-    TextureCache = PIXI.utils.TextureCache,
-    Sprite = PIXI.Sprite,
-    Text = PIXI.Text,
-    TextStyle = PIXI.TextStyle;
-  let canvasWidth = window.innerWidth,
-    canvasHeight = window.innerHeight;
-
-  //创建一个pixi应用
-  let app = new Application({
-    width: canvasWidth,
-    height: canvasHeight,
-    antialiasing: true,
-    forceCanvas: true,
-  });
-  poster.value.appendChild(app.view);
-
-  app.loader
-    .add(`bg`, 'poster/bg.png')
-    .add(`poster`, 'poster/poster.png')
-    .add(`btnAward`, 'poster/btn-award.png')
-    .add(`btnShare`, 'poster/btn-share.png');
-
-  app.loader.load((_loader, _resources) => {
-    resources = _resources;
-    setup(_resources);
-  });
-
-  function setup(resources) {
-    //设置游戏场景容器
-    let gameScene = new Container();
-    app.stage.addChild(gameScene);
-    //设置背景
-    let bgSprite = new Sprite(resources.bg.texture);
-    bgSprite.width = canvasWidth;
-    bgSprite.height = canvasHeight;
-    gameScene.addChild(bgSprite);
-
-    let posterSprite = new Sprite(resources.poster.texture);
-    const posterWidth = canvasWidth * 0.82;
-    posterSprite.width = posterWidth;
-    posterSprite.height = (posterWidth / 869) * 1543;
-    posterSprite.x = 0.1 * canvasWidth;
-    posterSprite.y = (canvasHeight - (posterWidth / 869) * 1543) * 0.7;
-
-    gameScene.addChild(posterSprite);
-
-    const fontSize = (30 / 375) * canvasWidth;
-    let text = new Text(`恭喜获得`, {
-      fontSize: `${fontSize}px`,
-      fill: '#fff',
-      align: 'center',
-    });
-    text.y = posterSprite.height * 0.7 + posterSprite.y;
-    text.x = posterSprite.x + posterWidth * 0.3;
-
-    gameScene.addChild(text);
-
-    let btnShare = new Sprite(resources.btnShare.texture);
-    btnShare.width = (120 / 375) * canvasWidth;
-    btnShare.height = (btnShare.width / 375) * 143;
-    btnShare.y =
-      posterSprite.height * 0.97 + posterSprite.y - btnShare.height / 2;
-    btnShare.x = posterSprite.x + posterWidth * 0.53;
-    btnShare.interactive = true;
-    btnShare.on('touchend', () => {
-      sound.play();
-      takeScreenshot();
-    });
-
-    gameScene.addChild(btnShare);
-
-    let btnAward = new Sprite(resources.btnAward.texture);
-    btnAward.width = (120 / 375) * canvasWidth;
-    btnAward.height = (btnShare.width / 375) * 143;
-    btnAward.y =
-      posterSprite.height * 0.97 + posterSprite.y - btnShare.height / 2;
-    btnAward.x = posterSprite.x + posterWidth * 0.06;
-    btnAward.interactive = true;
-    btnAward.on('touchend', () => {
-      sound.play();
-      router.replace({ name: 'Prize' });
-    });
-
-    gameScene.addChild(btnAward);
-
-    // app.renderer.plugins.interaction.on('pointerdown', takeScreenshot);
-  }
-
-  function takeScreenshot() {
-    const url = app.renderer.plugins.extract
-      .canvas(app.stage)
-      // .toBlob((b) => {
-      //   // const a = document.createElement('a');
-      //   // document.body.append(a);
-      //   // a.download = 'screenshot';
-      //   // a.href = URL.createObjectURL(b);
-      //   console.log(`URL.createObjectURL(b)`, URL.createObjectURL(b));
-      //   imgUrl.value = URL.createObjectURL(b);
-      //   // a.click();
-      //   // a.remove();
-      // }, 'image/png');
-      .toDataURL('image/png', 1);
-    console.log('url', url);
-    imgUrl.value = url;
-  }
+  modalStatus.value = true;
 });
 </script>
